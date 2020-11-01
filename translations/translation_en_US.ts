048b2394837c482931c4e7ebafb7f459a7eac9cf
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2020-11-01 11:21:35 +0900

    基本構成

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>ごめんなさい</source>
+            <comment>Text</comment>
+            <translation type="unfinished">ごめんなさい</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>うんうん.......,,,,いいですねぇ</source>
+            <comment>Text</comment>
+            <translation type="unfinished">うんうん.......,,,,いいですねぇ</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>あっ</source>
+            <comment>Text</comment>
+            <translation type="unfinished">あっ</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say_1_2</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>ごめんなさい</source>
+            <comment>Text</comment>
+            <translation type="unfinished">ごめんなさい</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say_2_B</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>うちのペッパーが申しわけありません</source>
+            <comment>Text</comment>
+            <translation type="unfinished">うちのペッパーが申しわけありません</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say_2_B (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>おい！君は何をしているんだ！</source>
+            <comment>Text</comment>
+            <translation type="unfinished">おい！君は何をしているんだ！</translation>
+        </message>
+    </context>
+</TS>
